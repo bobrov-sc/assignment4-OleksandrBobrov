498d57392b3732da56c748abbaa6b95ddfd6009b
Oleksandr Bobrov <bobrov@sheridancollege.ca>
2024-08-07 14:16:47 -0400

    created components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {ListComponent} from "./list/list.component";
+import {DetailsComponent} from "./details/details.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'catalog', component: ListComponent},
+  {path: 'pets/:id', component: DetailsComponent},
+  {path: '', redirectTo: 'catalog'},
+  {path: '**', redirectTo: 'catalog'}
+];
